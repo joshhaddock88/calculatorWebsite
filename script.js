9e188843e2a4d29a32e37bb9bc9a8fe62d39359c
Joshua Haddock <joshhaddock88@gmail.com>
2021-02-16 19:23:44 -0800

    Write control statement to evaluate if operator is clicked multiple times

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const factorial = number => {
         product *= i;
     } return product;
 };
+//this function will call and execute the arithmatic function.
+const operate = (operator, a, b) => operator(a, b);
 
 //Used these two arrays in order to loop through and compare/assign value;
 const arithmaticOperators = [add, subtract, multiply, divide, power, factorial]
@@ -19,28 +21,23 @@ const arithmaticInnerText = ["+", "-", "*", "/", "x^", "x!"]
 //the vars num1 & num2 will stand in for (a, b) in ^ math functions.
 let num1 = 0;
 let num2 = 0;
-//need to make operator equal to ^ of the above functions when the corresponding button is clicked
 let operatorDisplay = null;
 let operator = null;
-let tempNum = '';
-
+let tempNum1 = '';
+let tempNum2 = '';
 const buttons = document.querySelectorAll('button');
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 const clear = document.getElementById('clear')
-//this function will call and execute the arithmatic function.
-const operate = (operator, a, b) => operator(a, b);
 
-//loop through all buttons
+
+//adds evenlisteners to all buttons.
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        //if the button contains the class of digit (see the HTML) and an operator hasn't been used,
-        //we change the text to the digit as a string, then convert that string into a number for our variable.
         if(button.classList.contains('digit') && operator === null) {
-            input.innerText += button.innerText;
-            num1 = Number(input.innerText);
-        //this bit doesn't precisely work yet. operator = button.value technically gives the right asnwer but as a string.
-        //So if I console log operator it might say "add" as the value, but it won't call the add function. See?
+            tempNum1 += button.innerText;
+            num1 = Number(tempNum1);
+            input.innerText = `${num1}`
         } else if (button.classList.contains('operator')) {
             operatorDisplay = button.innerText;
             for(i = 0; i < arithmaticOperators.length; i++) {
@@ -49,25 +46,38 @@ buttons.forEach((button) => {
                     break;
                 }
             }
+            //this should make it so that pressing an operator twice is like
+            //pressing the = sign. It doesn't work perfectly yet though.
+            //This needs to be tweaked.
             input.innerText = `${num1} ${operatorDisplay}`;
-        //to solve the problem of the numbers I used a little variable I'm calling tempNum so that I can retain the value of the
+            if(tempNum2 !== '0' && operator !== null) {
+                tempNum1 = num1;
+                output.innerText = operate(operator, num1, num2);
+                num1 = Number(output.innerText);
+                tempNum2 = '0';
+                input.innerText = `${num1} ${operatorDisplay}`
+            }
+        //to solve the problem of the numbers I used a little variable I'm calling tempNum2 so that I can retain the value of the
         //new number post operator while keeping the input screen looking unchanged.
         } else if (button.classList.contains('digit') && operator !== null) {
-            tempNum += button.innerText;
-            num2 = Number(tempNum);
+            tempNum2 += button.innerText;
+            num2 = Number(tempNum2);
             input.innerText = `${num1} ${operatorDisplay} ${num2}`
-            console.log(num1);
-            console.log(operator);
-            console.log(num2);
         } else if (button.classList.contains('equals')) {
+            tempNum1 = num1;
             output.innerText = operate(operator, num1, num2);
+            num1 = Number(output.innerText);
+            tempNum2 = '0';
+            input.innerText = `${tempNum1} ${operatorDisplay} ${num2}`
         }
     })
 })
+
 clear.addEventListener('click', () => {
     num1 = 0;
     num2 = 0;
-    tempNum = '';
+    tempNum1 = '';
+    tempNum2 = '';
     operator = null;
     operatorDisplay = null;
     input.innerText = '';
